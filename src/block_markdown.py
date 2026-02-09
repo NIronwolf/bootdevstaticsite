65db3106f64b57851ae0559ab8c7e166fba8533c
Nyplid Ironwolf <gitlab-ce@nironwolf.com>
2026-02-08 21:14:46 -0700

    Ch4L2

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,16 @@
+import re
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
 def markdown_to_blocks(markdown: str):
     splits = markdown.split("\n\n")
     blocks = []
@@ -5,3 +18,34 @@ def markdown_to_blocks(markdown: str):
         if split != "":
             blocks.append(split.strip())
     return blocks
+
+
+# ordered_list_counter = 1
+
+
+def block_to_block_type(block: str) -> BlockType:
+    if re.findall(r"^(#{1,6}) ", block):
+        return BlockType.HEADING
+    if re.findall(r"^```(?:\n[\s\S]*?)?\n```$", block):
+        return BlockType.CODE
+
+    lines = block.split("\n")
+
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, block_to_block_type, BlockType
 
 
 class TestBlockMarkdown(unittest.TestCase):
@@ -46,6 +46,57 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_block_to_block_type(self):
+        blocks = [
+            "# Heading 1",
+            "## Heading 2",
+            "### Heading 3",
+            "#### Heading 4",
+            "##### Heading 5",
+            "###### Heading 6",
+            "This is a paragraph.",
+            "> This is a quote.",
+            "- This is an unordered list item.",
+            "1. This is an ordered list item.",
+        ]
+        self.assertEqual(block_to_block_type(blocks[0]), BlockType.HEADING)
+        self.assertEqual(block_to_block_type(blocks[1]), BlockType.HEADING)
+        self.assertEqual(block_to_block_type(blocks[2]), BlockType.HEADING)
+        self.assertEqual(block_to_block_type(blocks[3]), BlockType.HEADING)
+        self.assertEqual(block_to_block_type(blocks[4]), BlockType.HEADING)
+        self.assertEqual(block_to_block_type(blocks[5]), BlockType.HEADING)
+        self.assertEqual(block_to_block_type(blocks[6]), BlockType.PARAGRAPH)
+        self.assertEqual(block_to_block_type(blocks[7]), BlockType.QUOTE)
+        self.assertEqual(block_to_block_type(blocks[8]), BlockType.UNORDERED_LIST)
+        self.assertEqual(block_to_block_type(blocks[9]), BlockType.ORDERED_LIST)
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+        block = "- list\n- items"
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+        block = "1. list\n- not a list"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+        block = "- list\n1. not a list"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+        block = "> quote\nnot a quote"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+        block = '```json\n{\n  "key": "value"\n}\n```'  # code type not supported
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+        # ordered list must start at 1
+        block = "2. not starting at one\n3. still ordered"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+        # ordered list must not skip numbers
+        block = "1. first\n3. skipped two"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
 
 
 if __name__ == "__main__":
